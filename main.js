ce4553e3ae21011f0728476bb1a5b94ba354a338
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-06-03 15:50:24 -0300

    melhorando a experiencia do  usuario adicionando efeitos ao usar o teclado

--- main.js
+++ main.js
@@ -6,12 +6,25 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const tecla = listaDeTeclas[contador]
-    const instrumento = tecla.classList[1]
-    const idAudio = `#som_${instrumento}`
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
-        tocaSom(idAudio)
+        tocaSom(idAudio);
     }
-    
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code)
+
+        if (evento.code === "Space" || evento.code === "Enter") {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
