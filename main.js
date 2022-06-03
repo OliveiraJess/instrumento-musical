76d62c7265cb3d53a89679233aef8c7d0cb4bcb8
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-06-03 14:23:38 -0300

    incluindo as teclas numa lista e inplementando a funcao tocaSom

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function tocaSomPom() {   
+function tocaSom(idElementoAudio) {   
 
-    document.querySelector('#som_tecla_pom').play()
+    document.querySelector(idElementoAudio).play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla')
 
 
+let contador = 0;
 
+while (contador < listaDeTeclas.length) {   
+    listaDeTeclas[contador].onclick = tocaSom
+
+    contador++
+}
