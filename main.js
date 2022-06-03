0a1caa9a7139ffe281f9271a433ef348cc092341
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-06-03 11:01:29 -0300

    vinculando a funcao tocaSomPom ao onclick

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function tocaSomPom() {
-    document.querySelector('.tecla_pom')
+function tocaSomPom() {   
 
     document.querySelector('#som_tecla_pom').play()
 }
 
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
 
