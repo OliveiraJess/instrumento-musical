25e29ff5223a3bdbbbd1e1e36d778e3e1758456f
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-06-03 14:55:47 -0300

    substituindo while por for para melhorar o codigo

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 function tocaSom(idElementoAudio) {
-
-    document.querySelector(idElementoAudio).play()
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador]
-
     const instrumento = tecla.classList[1]
-
     const idAudio = `#som_${instrumento}`
 
     tecla.onclick = function () {
         tocaSom(idAudio)
     }
-
-    contador++
-
+    
 }
